b14a1613253426e9a404b26fa76e8ed9c01a6ccc
a19z5yk <a19z5yk@aiit.ac.jp>
2020-07-04 10:03:50 +0000

    travis ci setting

--- gcdtest.sh
+++ gcdtest.sh
@@ -5,7 +5,7 @@ if
 	[ -n "$(echo "$2" | grep  '^[1-9][0-9]*$')" ]	
 then
 	result=$(./gcd.sh $1 $2)
-	echo "gcd は $1 and $2 is $result !"
+	echo " $result "
 	exit 0
 else
 	echo "2つの自然数を入力してください" 1>&2
