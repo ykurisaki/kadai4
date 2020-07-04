ba27e80bfacfe0d05f6148147ee3a6239cc08a35
a19z5yk <a19z5yk@aiit.ac.jp>
2020-07-04 09:41:24 +0000

    travis ci setting

--- gcd.sh
+++ gcd.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-if
-	[ -n "$(echo "$1" | grep  '^[1-9][0-9]*$')" ] &&
-	[ -n "$(echo "$2" | grep  '^[1-9][0-9]*$')" ]	
-then
-	result=$(./euclid.sh $1 $2)
-	echo "The GCF of $1 and $2 is $result !"
-	exit 0
-else
-	echo "0以外の2つの数値を入力してください" 1>&2
-	exit 1
+#echo "2個の整数を入力してください"
+#read str1 str2
+
+if [ $# -ne 2 ]; then 		# 2個でない場合
+echo "$#個の引数が入力されています。引数の数が不正です。" 	# エラーメッセージ
+exit 1 # 終了ステータス
 fi
+
+expr $1 + $2 > /dev/null 2>&1 	# 数字以外の入力の場合
+if [ $? -ge 2 ]; then 		
+echo "$1と$2が入力されました。2個の整数を入力してください" 	# エラーメッセージ
+exit 1 		# 終了ステータス
+fi
+
+echo "$1と$2の最大公約数は↓" 
+yes $1 $2 | awk '{print $1/NR RS $2/NR}' | grep -Fv --line-buffered . | awk 'a[$1]++{print;exit}'
